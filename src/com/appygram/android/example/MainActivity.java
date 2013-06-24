e75236ac0c2578d5a5f80a24309d5141c8bcd600
Rob Heittman <heittman.rob@gmail.com>
2013-06-24 18:45:23 -0400

    Whitespace corrections

--- src/com/appygram/android/example/MainActivity.java
+++ src/com/appygram/android/example/MainActivity.java
@@ -23,7 +23,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	private final static String APPYGRAM_API_KEY = "your-api-key-here";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,28 +36,28 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	private class SendAppygramTask extends AsyncTask<URL, Integer, Long> {
 		private Map<String,String> params;
 
 		private String join(List<String> aArr, String sSep) {
-		    StringBuilder sbStr = new StringBuilder();
-		    for (int i = 0, il = aArr.size(); i < il; i++) {
-		        if (i > 0)
-		            sbStr.append(sSep);
-		        sbStr.append(aArr.get(i));
-		    }
-		    return sbStr.toString();
+			StringBuilder sbStr = new StringBuilder();
+			for (int i = 0, il = aArr.size(); i < il; i++) {
+				if (i > 0)
+					sbStr.append(sSep);
+				sbStr.append(aArr.get(i));
+			}
+			return sbStr.toString();
 		}
-		
+
 		protected SendAppygramTask(Map<String,String> params){
 			super();
 			this.params = params;
 			params.put("api_key", APPYGRAM_API_KEY);
 		}
 
-	    protected Long doInBackground(URL... urls) {
-	    	Long result = 0L;
+		protected Long doInBackground(URL... urls) {
+			Long result = 0L;
 			try{
 				URL url = new URL("https://arecibo.appygram.com/appygrams");
 				HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
@@ -86,15 +86,15 @@ public class MainActivity extends Activity {
 			return result;
 		}
 
-	    protected void onPostExecute(Long result) {
-	        // TODO: show that something happened
-	    }
+		protected void onPostExecute(Long result) {
+			// TODO: show that something happened
+		}
 	}
 
 	private String getUIString(int id){
 		return ((TextView) findViewById(id)).getText().toString();
 	}
-	
+
 	public void send(View source) {
 		// Collect parameters from the UI
 		Map<String, String> params = new HashMap<String, String>();
